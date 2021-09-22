44f7e4ad7620add0ca721cf5d6071aacf3f0ecbf
Eric Yi <erickanginyi@gmail.com>
2021-09-22 12:23:10 -0400

    commit tree 3cb47fbe2fad308830fa78cfe3df511c93042e51

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,38 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(project => {
+          return (
+            <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+/*
+projects: [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    technologies: ["Rails", "Bootstrap CSS"],
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    technologies: ["React", "Redux"],
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    technologies: ["React", "Redux", "Rails"],
+  },
+],
+*/
